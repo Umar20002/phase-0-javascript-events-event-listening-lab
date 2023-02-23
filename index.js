9322795af3a4179c91727ab99e22abce9984b532
Im Umar <iamumar@gm>
2023-02-22 22:03:36 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function addingEventListener() {
-const input = document.getElementById('button');
-input.addEventListener('click', function() {
-    alert('I was clicked!');
-});
+function addingEventListener(){
+    const input = document.getElementById('button')
+    input.addEventListener('click', function(){
+        alert('You just clicked!')
+    })
 }
+addingEventListener()
